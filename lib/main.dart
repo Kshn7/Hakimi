6250a8f2450c470ca6d1a1ce36886c04057c7b63
natasha <nurnatashaismail.123@gmail.com>
2024-12-12 10:55:30 +0800

    git commit -m "update"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'booking_calendar.dart'; // Import BookingCalendarPage
+import 'calendar/calendar.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BookingCalendarPage(), // Set this as the home temporarily
+      home: BookingCalendarPage(), // Set this as the home temporarily
       debugShowCheckedModeBanner: false,
     );
   }
