8639a6d8d1573db9598e02b924aa889877aa2ccf
tomyamGTX <hakimimdnoor90@gmail.com>
2024-12-17 10:32:04 +0800

    add generate pdf button and function

--- lib/receipt/receipt.dart
+++ lib/receipt/receipt.dart
@@ -1,4 +1,11 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 class ReceiptGenerator extends StatefulWidget {
   const ReceiptGenerator({super.key});
@@ -8,37 +15,97 @@ class ReceiptGenerator extends StatefulWidget {
 }
 
 class _ReceiptGeneratorState extends State<ReceiptGenerator> {
+  // Create a GlobalKey to capture the widget
+  final key = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          BigBox(),
-          SizedBox(height: 10),
-          Center(
+          RepaintBoundary(key: key, child: const BigBox()),
+          const SizedBox(height: 10),
+          const Center(
             child: Text(
               'Preview',
               style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
             ),
           ),
-          SizedBox(height: 8),
-          CustomInputField(
+          const SizedBox(height: 8),
+          const CustomInputField(
             initialValue: '',
             hintText: 'Customer details',
           ),
-          SizedBox(height: 6),
-          CustomInputField(
+          const SizedBox(height: 6),
+          const CustomInputField(
             hintText: 'Customer number',
           ),
-          SizedBox(height: 12),
-          ReceiptDetails(),
-          SizedBox(height: 12),
-          GenerateButton(),
+          const SizedBox(height: 12),
+          const ReceiptDetails(),
+          const SizedBox(height: 24),
+          Center(
+            child: ElevatedButton.icon(
+              onPressed: () {
+                //generate receipt as pdf
+                _generateAndPrintPDF();
+              },
+              icon: const Icon(Icons.autorenew, color: Colors.white, size: 12),
+              label: const Text(
+                'Generate',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+// Function to generate and print PDF
+  void _generateAndPrintPDF() async {
+    final pdf = pw.Document();
+
+    // Wait for the widget to be painted and then capture the image
+    await Future.delayed(Duration(milliseconds: 100));
+
+    // Capture the image as a byte array
+    final imageBytes = await _captureWidgetToImage(key);
+
+    // Add a page to the PDF and insert the captured image
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Image(pw.MemoryImage(
+              imageBytes,
+            )),
+          );
+        },
+      ),
+    );
+
+    // Use the printing package to preview and print the PDF
+    await Printing.layoutPdf(
+      onLayout: (PdfPageFormat format) async {
+        return pdf.save(); // Return the PDF bytes to print or preview
+      },
+    );
+  } // Function to capture the widget as an image in bytes
+
+  Future<Uint8List> _captureWidgetToImage(GlobalKey key) async {
+    RenderRepaintBoundary boundary =
+        key.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    var image = await boundary.toImage(pixelRatio: 3.0);
+    ByteData? byteData = await image.toByteData(format: ImageByteFormat.png);
+    return byteData!.buffer.asUint8List();
   }
 }
 
@@ -189,8 +256,8 @@ class BigBox extends StatelessWidget {
                 ),
                 const SizedBox(height: 8),
                 const LowerLabelInputBoxes(),
-                const SizedBox(height: 10),
-                const ResitRasmiButton(),
+                // const SizedBox(height: 10),
+                // const ResitRasmiButton(),
                 const SizedBox(height: 4),
                 const Align(
                   alignment: Alignment.centerLeft,
@@ -346,32 +413,6 @@ class ResitRasmiButton extends StatelessWidget {
             fontSize: 10,
             fontWeight: FontWeight.bold,
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class GenerateButton extends StatelessWidget {
-  const GenerateButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton.icon(
-        onPressed: () {},
-        icon: const Icon(Icons.autorenew, color: Colors.white, size: 12),
-        label: const Text(
-          'Generate',
-          style: TextStyle(
-            fontSize: 10,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue,
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
         ),
       ),
     );
