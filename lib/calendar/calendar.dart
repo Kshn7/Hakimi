51dc2380139efa6939f614050ec6b820d713890a
tomyamGTX <hakimimdnoor90@gmail.com>
2024-12-30 19:13:59 +0800

    fix calendar. Todo save holiday to local based on year

--- lib/calendar/calendar.dart
+++ lib/calendar/calendar.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:homestay_form/api/api_holiday.dart';
 
 class BookingCalendarPage extends StatefulWidget {
   const BookingCalendarPage({super.key});
@@ -14,8 +12,7 @@ class _BookingCalendarPageState extends State<BookingCalendarPage> {
   int year = DateTime.now().year;
   int month = DateTime.now().month;
   int? selectedDay;
-  List<String> holidayDates = [];
-
+  List holidayDates = [];
   final Map<String, List<String>> bookings = {
     "2024-10-15": ["10:00 AM - John Doe", "2:00 PM - Jane Smith"],
     "2024-10-18": ["11:30 AM - Alice Brown"],
@@ -24,54 +21,54 @@ class _BookingCalendarPageState extends State<BookingCalendarPage> {
 
   @override
   void initState() {
+    fetchHoliday(year.toString());
     super.initState();
-    fetchHolidays();
   }
 
-  Future<void> fetchHolidays() async {
-    final uri = Uri.https('holidayapi.com', '/v1/holidays', {
-      'country': 'MY',
-      'year': year.toString(),
-      'month': month.toString(),
-      'key': '0ba24f61-9795-4f75-b4c9-71afc2e556c0', // Your API key
-      'public': 'true', // Optional filter for public holidays only
-      'pretty': 'true' // Optional to prettify the response
-    });
-
-    try {
-      final response = await http.get(uri);
-
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        setState(() {
-          holidayDates = (data['holidays'] as List)
-              .map((holiday) => holiday['date'] as String)
-              .toList();
-        });
-      } else {
-        throw Exception('Failed to fetch holidays: ${response.body}');
-      }
-    } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Error fetching holidays: $error"),
-          backgroundColor: Colors.red,
-        ),
-      );
-    }
-  }
+  // Future<void> fetchHolidays() async {
+  //   final uri = Uri.https('holidayapi.com', '/v1/holidays', {
+  //     'country': 'MY',
+  //     'year': year.toString(),
+  //     'month': month.toString(),
+  //     'key': '0ba24f61-9795-4f75-b4c9-71afc2e556c0', // Your API key
+  //     'public': 'true', // Optional filter for public holidays only
+  //     'pretty': 'true' // Optional to prettify the response
+  //   });
+  //
+  //   try {
+  //     final response = await http.get(uri);
+  //
+  //     if (response.statusCode == 200) {
+  //       final data = jsonDecode(response.body);
+  //       setState(() {
+  //         holidayDates = (data['holidays'] as List)
+  //             .map((holiday) => holiday['date'] as String)
+  //             .toList();
+  //       });
+  //     } else {
+  //       throw Exception('Failed to fetch holidays: ${response.body}');
+  //     }
+  //   } catch (error) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(
+  //         content: Text("Error fetching holidays: $error"),
+  //         backgroundColor: Colors.red,
+  //       ),
+  //     );
+  //   }
+  // }
 
   void incrementMonth() {
     setState(() {
       if (month == 12) {
         month = 1;
         year += 1;
+        fetchHoliday(year.toString());
       } else {
         month += 1;
       }
       selectedDay = null;
     });
-    fetchHolidays();
   }
 
   void decrementMonth() {
@@ -79,37 +76,38 @@ class _BookingCalendarPageState extends State<BookingCalendarPage> {
       if (month == 1) {
         month = 12;
         year -= 1;
+        fetchHoliday(year.toString());
       } else {
         month -= 1;
       }
       selectedDay = null;
     });
-    fetchHolidays();
   }
 
   @override
   Widget build(BuildContext context) {
     int daysInMonth = DateTime(year, month + 1, 0).day;
     int firstDayIndex = DateTime(year, month, 1).weekday % 7;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Booking Calendar'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: decrementMonth,
-          ),
-          IconButton(
-            icon: const Icon(Icons.arrow_forward),
-            onPressed: incrementMonth,
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+    var size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        width: size.width,
+        height: size.height,
         child: Column(
           children: [
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: decrementMonth,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.arrow_forward),
+                  onPressed: incrementMonth,
+                ),
+              ],
+            ),
             Text(
               '${monthNames[month - 1]} $year',
               style: TextStyle(
@@ -178,7 +176,7 @@ class _BookingCalendarPageState extends State<BookingCalendarPage> {
               ),
             ),
             // Your holidays table
-            HolidaysTable(holidayDates: holidayDates),
+            HolidaysTable(holidayDates: holidayDates, selectedMonth: month),
           ],
         ),
       ),
@@ -224,12 +222,31 @@ class _BookingCalendarPageState extends State<BookingCalendarPage> {
     'November',
     'December',
   ];
+
+  Future<void> fetchHoliday(String year) async {
+    var holiday = await APIHoliday().fetchHoliday(year);
+    holidayDates = holiday;
+    setState(() {});
+    //   {
+    //     "name": "New Year's Day",
+    //   "date": { "month": 1, "day": 1 },
+    //   "formattedDate": "1 Jan",
+    //   "weekday": { "numeric": 5, "name": "Friday" },
+    //   "is_public": false,
+    //   "is_observed": false,
+    //   "is_religious": false,
+    //   "type": "national holiday"
+    // }
+    setState(() {});
+  }
 }
 
 class HolidaysTable extends StatelessWidget {
-  final List<String> holidayDates;
+  final List holidayDates;
+  final int selectedMonth;
 
-  const HolidaysTable({super.key, required this.holidayDates});
+  const HolidaysTable(
+      {super.key, required this.holidayDates, required this.selectedMonth});
 
   @override
   Widget build(BuildContext context) {
@@ -238,8 +255,11 @@ class HolidaysTable extends StatelessWidget {
         itemCount: holidayDates.length,
         itemBuilder: (context, index) {
           final holidayDate = holidayDates[index];
+          final month = holidayDate['date']['month'];
+          if (month != selectedMonth) return const SizedBox.shrink();
           return ListTile(
-            title: Text(holidayDate),
+            title: Text(holidayDate['name']),
+            subtitle: Text("${holidayDate['date']['day']}/$month"),
             trailing: const Icon(Icons.calendar_today),
           );
         },
